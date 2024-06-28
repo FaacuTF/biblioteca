6a29777b25065df3596d0687d6d90e1929c5262c
FaacuTF <fialafacu@gmail.com>
2024-06-28 11:46:22 -0300

    se crearon las clases biblioteca y libro, y se inicio con la estructura de biblioteca

---
+++ src/clases/Biblioteca.java
@@ -0,0 +1,12 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Libro> Libro;
+    public void agregarLibro(Libro libro){}
+    public void listarTodos()
+    public Libro buscarLibro (String titulo)
+        this.Libro = new.ArrayList<Libro>()
+    }
+}
---
+++ src/clases/Libro.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class Libro {
+    private id (int)
+    private titulo (String)
+    private autor (String)
+    private numDePag (int)
+
+}
