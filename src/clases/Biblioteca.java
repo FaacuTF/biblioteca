25299d45b072aef55fd2431e2942726b96b017ff
FaacuTF <fialafacu@gmail.com>
2024-07-05 11:50:34 -0300

    Avanzamos en el armado de la clase Libro y Biblioteca

--- src/clases/Biblioteca.java
+++ src/clases/Biblioteca.java
@@ -1,12 +1,30 @@
 package clases;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Biblioteca {
-    private ArrayList<Libro> Libro;
+    Scanner scanner = new Scanner(System.in);
+    private ArrayList<Libro> libros;
     public void agregarLibro(Libro libro){}
-    public void listarTodos()
-    public Libro buscarLibro (String titulo)
-        this.Libro = new.ArrayList<Libro>()
+    public void listarTodos(){}{
+        this.libros = new ArrayList<Libro>();
     }
-}
+    public void agregarLibros(Libro libro){
+        libros.add(libro);
+        System.out.printf("Libro agregado."+ libro.toString());
+    }
+    public void crearLibro(){
+        System.out.printf("Ingrese el titulo del libro");
+        String titulo = scanner.nextLine();
+        System.out.printf("Ingrese el autor del libro");
+        String autor = scanner.nextLine();
+        System.out.printf("Ingrese la cantidad de paginas");
+        Integer cantidadPags = scanner.nextInt();
+
+        Libro nuevoLibro = new Libro(titulo,autor,cantidadPags);
+
+        libros.add(nuevoLibro);
+    }
+
+    public void buscarLibro(String titulo){}}
--- src/clases/Libro.java
+++ src/clases/Libro.java
@@ -1,9 +1,23 @@
 package clases;
 
-public class Libro {
-    private id (int)
-    private titulo (String)
-    private autor (String)
-    private numDePag (int)
+import java.lang.annotation.Native;
+import java.util.Random;
 
+public class Libro {
+    Random random = new Random();
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private Integer cantidadPags;
+
+    public Libro(String titulo, String autor, Integer cantidadPags) {
+        this.id = random.nextInt(101);
+        this.titulo = titulo;
+        this.autor = autor;
+        this.cantidadPags = cantidadPags;
+    }
+    public String toString() {
+        return titulo +
+                "id" + id + "autor" + autor + '\'' + "cantidadPags" + cantidadPags;
+    }
 }
